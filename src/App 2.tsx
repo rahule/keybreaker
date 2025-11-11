4c41b30374c4d7ec2ae0dfa7bfec7326c7aea2d3
rahul edathil <rahuledat6@gmail.com>
2025-11-11 20:07:17 +0530

    code update

--- src/App 2.tsx
+++ src/App 2.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import Logo from './images/Logo-Key.svg';
+import Logo from './Logo-Key.svg'; // Make sure this path is correct and matches the folder structure exactly
 
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Typing Speed Test</h1>
+
+      {/* Ensure Logo-Key.svg exists in ./images/ */}
       <img src={Logo} alt="Logo" width={150} />
     </div>
   );
